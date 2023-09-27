43d73db50c3f667592307afa90be7e5169bd6da0
Sandra Lopez <slopeziglesias@danielcastelao.org>
2023-09-27 09:08:17 +0200

    empezando con android studio

---
+++ app/src/main/java/com/dam/primera/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.dam.primera
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.dam.primera.ui.theme.PrimeraTheme
+
+class MainActivity : ComponentActivity() {
+
+    val TAG : String = "estado"
+    var name : String = "Android"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PrimeraTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //Greeting("Android")
+                    Greeting(name)
+                }
+            }
+        }
+        Log.d(TAG, "estoy en onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "estoy en onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "estoy en onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "estoy en onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "estoy en onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "estoy en onDestroy")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Saludo(name: String, modifier: Modifier = Modifier){
+    Text(
+        text = "Hola $name!",
+        fontSize = 100.sp,
+        color = Color.Blue,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PrimeraTheme {
+        Greeting("Android")
+    }
+}
